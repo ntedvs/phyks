58f2a6922b4a2fdd6ea23b76add1651667b8b447
Nate <nate@qstreet.org>
2025-06-02 21:33:56 -0400

    begin script

---
+++ phyks.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Phyks
+// @namespace    https://github.com/ntedvs/phyks
+// @version      0.1.0
+// @description  Fix the interface that is Outlook.
+// @author       Nathaniel Davis
+// @match        https://outlook.office.com/mail/
+// ==/UserScript==
+
+const phyks = () => {
+  console.log("Hello, World!")
+}
+
+phyks()
